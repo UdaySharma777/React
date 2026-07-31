ab323641e56f530a7b55400f3dbbe8399b46fa49
UdaySharma777 <uday777sh@gmail.com>
2026-07-31 17:19:36 +0530

    react vite understanding

---
+++ 01-vite-react/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+
+function App() {
+  
+
+  return( 
+    <h1> React with vite | Uday</h1>
+  )
+}
+
+export default App
